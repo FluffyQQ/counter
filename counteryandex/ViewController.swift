084517895743d166b55a135a83f00ac6f89a677a
fluffyqq <fluffytime@yandex.ru>
2025-07-03 05:39:34 +0300

    Initial Commit

---
+++ counteryandex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  counteryandex
+//
+//  Created by Максим Фролов on 03.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ counteryandexTests/counteryandexTests.swift
@@ -0,0 +1,17 @@
+//
+//  counteryandexTests.swift
+//  counteryandexTests
+//
+//  Created by Максим Фролов on 03.07.2025.
+//
+
+import Testing
+@testable import counteryandex
+
+struct counteryandexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
